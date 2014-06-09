849c088bfa0217078ff69faa213e6f6eb672c58e
kaz-yos <kazky@mac.com>
2014-06-08 22:00:06 -0400

    Solve chapter 3 max duplicated strings

--- web_db_67_functional_programming.clj
+++ web_db_67_functional_programming.clj
@@ -22,15 +22,49 @@
 ;;
 ;; (defn max-dup-str [st]
 ;;   :str)
+;;
+(defn suffixes [st]
+  (let [st-seq (seq st)]
+    (loop [acc []
+           sseq st-seq]
+      ;;
+      (if (empty? sseq)
+        (conj acc '())
+        (recur (conj acc sseq) (rest sseq))))))
+;;
+(suffixes "mississippi")
+;;
+(defn sorted-suffixes [st]
+  (sort (map (fn [sseq] (clojure.string/join sseq)) (suffixes st))))
+;;
+(sorted-suffixes "mississippi")
+;;
+(defn pairs [st]
+  (let [sorted-suff (sorted-suffixes st)]
+    (partition-all 2 (interleave sorted-suff (rest sorted-suff)))))
+;;
+(pairs "mississippi")
+;;
+(defn match-length [[a b]]
+  (loop [acc 0
+         ssta (seq a)
+         sstb (seq b)]
+    (cond
+     (or (empty? ssta) (empty? sstb)) acc
+     (not= (first ssta) (first sstb)) acc
+     :else (recur (inc acc) (rest ssta) (rest sstb)))))
+;;
+(map match-length (pairs "mississippi"))
+;;
+;;
 (defn max-dup-str [st]
-  (mapcat str (reverse (reductions (fn [a b] (reverse (conj a b))) [] (reverse st)))))
-
-
-
+  (let [max-match-pair (last (sort-by  match-length (pairs st)))]
+    (clojure.string/join (take (match-length max-match-pair) (first max-match-pair)))))
+;;
 (max-dup-str "mississippi")
 ;;
 (clojure.test/is (= (max-dup-str "mississippi")
                     "issi"))
 (clojure.test/is (= (max-dup-str "Ask not what your country can do for you, but what you can do for your country.")
-                    "can do for you"))
+                    " can do for you"))
 
